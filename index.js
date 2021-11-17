ad69aa83659ba06b83a312771c4c7fa23346ef1f
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2021-11-17 12:25:50 -0600

    Explorar cursos

--- index.js
+++ index.js
@@ -72,6 +72,17 @@ app.get('/estatusServicio/:id', (req, res) => {
 })
 })
 
+//Metodo para mostar el nombre de la sesion e ir a explorar cursos
+app.get('/explora/:id', (req, res) => {
+	const { id } = req.params;
+	console.log('Correo recibido: ' + id)
+	usuario.findOne({correo: id}, function (err, usuarios) {
+	console.log("valor del corre: " + id)
+	console.log("UsarioEncontrado: " + usuarios); 
+	res.render('explora', { usuarios: usuarios, idUser: id }  )
+})
+})
+
 //Metodo para mostar el nombre de la sesion e ir subir curso
 app.get('/subirCurso/:id', (req, res) => {
 	const { id } = req.params;
